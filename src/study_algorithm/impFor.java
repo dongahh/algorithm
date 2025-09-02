9afb944c50710a35b59fcf3178399da7cf627382
dong c <dchoi1211@gmail.com>
2025-09-02 23:07:20 +0900

    202509

--- src/study_algorithm/impFor.java
+++ src/study_algorithm/impFor.java
@@ -1,16 +1,20 @@
 package study_algorithm;
 
+import java.io.Console;
+
 public class impFor {
 
 	public static void main(String[] args) {
-		//향상된 for문
-		String[][] arr = {{"1-1","1-2","1-3","1-4","1-5"},{"2-1","2-2","2-3","2-4","2-5"}};
- 		
-		for(String arrs : arr[1]) {
+		String str = "apple";
 		
-				System.out.println(arrs);
-			
-		}
+		str.isEmpty();
+		str.charAt(0);
+		str.indexOf("a");
+		str.lastIndexOf("p");
+		str.substring(3);
+		
+		
+		
 
 	}
 
